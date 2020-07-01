654c4eb3c18c15bf8135d5055ee5bb4fe21fa675
Pavel <pavellapunov@inbox.ru>
2020-07-01 14:28:54 +0300

    project for second check

--- source/js/contacts.js
+++
@@ -1,1 +0,0 @@
-function initMap(){var e={lat:59.9385,lng:30.3229},a=new google.maps.Map(document.getElementById("map"),{zoom:16,disableDefaultUI:!1,center:e});new google.maps.Marker({position:e,map:a,title:"antarctic",icon:{url:"img/mark.svg",scaledSize:new google.maps.Size(18,22)}})}
